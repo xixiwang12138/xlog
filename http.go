c9bb032647583f515aeb7c7df14f61af02370e7e
xixiwang <fewzh@mail.scut.edu.cn>
2023-03-05 01:23:08 +0800

    fix: 优化输出

--- http.go
+++ http.go
@@ -30,7 +30,7 @@ func TracingLogger(ctxGenerator func() context.Context) gin.HandlerFunc {
 		ctx.Header(ReqHeader, traceId)
 		end := time.Now()
 		xl.SetFlags(Ldate | Ltime)
-		xl.Infof("%-30s %-30s  ===>  %d  %dms\n", ctx.Request.Method, ctx.Request.RequestURI, ctx.Request.Response.StatusCode, end.UnixMilli()-start.UnixMilli())
+		xl.Infof("%-30s %-30s  ===>  %dms\n", ctx.Request.Method, ctx.Request.RequestURI, ctx, end.UnixMilli()-start.UnixMilli())
 		xl.SetFlags(Ldefault)
 	}
 }
