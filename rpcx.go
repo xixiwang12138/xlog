fca807936f2ef0712a0fc42ba86fb7a55d485ed2
xixiwang <fewzh@mail.scut.edu.cn>
2023-03-05 15:31:12 +0800

    fix: 修复bug

--- rpcx.go
+++ rpcx.go
@@ -6,7 +6,8 @@ import (
 )
 
 var (
-	RPCXServerPreHandle = &serverPreHandle{}
+	RPCXServerPreHandle   = &serverPreHandle{}
+	RPCXServerAfterHandle = &serverAfterHandle{}
 )
 
 type serverPreHandle struct{}
@@ -18,7 +19,25 @@ func (handler *serverPreHandle) PostReadRequest(ctx context.Context, r *protocol
 		if ok {
 			xl.reqId = reqId
 		}
-		xl.Infof("[RPC Handle] %s, Args: %s \n", string(r.Payload))
+		xl.Infof("[RPC Handle] %s, Args: %s \n", r.ServiceMethod, string(r.Payload))
+	}
+	return nil
+}
+
+type serverAfterHandle struct{}
+
+func (handler *serverAfterHandle) PreWriteResponse(ctx context.Context, r *protocol.Message, resp *protocol.Message, err error) error {
+	reqId, ok := r.Metadata[ReqHeader]
+	if !r.IsOneway() && !r.IsHeartbeat() {
+		xl := NewLogger()
+		if ok {
+			xl.reqId = reqId
+		}
+		if err != nil {
+			xl.Error("[Rpc Server Internal] Handle: %s, error: %s", r.ServiceMethod, err.Error())
+			return nil
+		}
+		xl.Infof("[RPC Handle] %s, Response: %s \n", r.ServiceMethod, string(resp.Payload))
 	}
 	return nil
 }
